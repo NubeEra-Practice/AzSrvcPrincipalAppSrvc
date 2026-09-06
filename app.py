0f5c6d299860e8dede01ce30a3dbc579538413ae
Syed Mujahed Hussaini <mujahed.mca@gmail.com>
2026-09-06 12:24:46 +0000

    V1.1 - Deploying Web App

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "Hello from GitHub Actions + Azure!",
+        "status": "running"
+    })
+
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "healthy"
+    })
+
+
+@app.route("/api/users")
+def users():
+    return jsonify({
+        "users": [
+            {
+                "id": 1,
+                "name": "John"
+            },
+            {
+                "id": 2,
+                "name": "Alice"
+            }
+        ]
+    })
+
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=8000
+    )
